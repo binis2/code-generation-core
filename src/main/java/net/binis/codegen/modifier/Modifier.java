998d6f91a87e30eb0c67ee8ffa7b3dafc6e7ff13
Binis <binisbelev@gmail.com>
2020-11-10 13:18:04 +0200

    Added Final annotation

--- src/main/java/net/binis/codegen/modifier/Modifier.java
+++ src/main/java/net/binis/codegen/modifier/Modifier.java
@@ -1,6 +1,6 @@
 package net.binis.codegen.modifier;
 
 @FunctionalInterface
-public interface Modifier {
-    void setObject(Object object);
+public interface Modifier<T> {
+    void setObject(T object);
 }
