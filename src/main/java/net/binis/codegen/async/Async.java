ed52ffed91e554a991fc1b8f060c3dddf3e37d8d
BINIS\binis <binisbelev@gmail.com>
2025-12-25 21:55:01 +0200

    Added sleep routine to Async

--- src/main/java/net/binis/codegen/async/Async.java
+++ src/main/java/net/binis/codegen/async/Async.java
@@ -9,9 +9,9 @@ package net.binis.codegen.async;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,7 @@ import net.binis.codegen.async.executor.CodeExecutor;
 import net.binis.codegen.async.executor.impl.AsyncExecutorImpl;
 import net.binis.codegen.factory.CodeFactory;
 
+import java.time.Duration;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
 
@@ -32,6 +33,18 @@ public class Async {
     static {
         CodeFactory.registerType(AsyncDispatcher.class, CodeFactory.singleton(CodeExecutor.defaultDispatcher()));
         CodeFactory.registerType(AsyncExecutor.class, AsyncExecutorImpl::new);
+    }
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static void sleep(Duration duration) {
+        sleep(duration.toMillis());
     }
 
     @SuppressWarnings("unchecked")
